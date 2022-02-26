68611518be19fbff89a291daeda30d80c185af73
Keoni <han24@up.edu>
2022-02-25 16:59:20 -0800

    Added -first instance of game state -deep copy call of first instance -added for loop for players fleet in deep copy constructor

--- app/src/main/java/com/example/proje_battleshiptest/MainActivity.java
+++ app/src/main/java/com/example/proje_battleshiptest/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button runTest = findViewById(R.id.runTest);
+        EditText multiLine = findViewById(R.id.editTextTextMultiLine);
 
         View.OnClickListener buttonHandler = new View.OnClickListener() {
             @Override
             public void onClick(View button) {
+                //Clears Text
+                multiLine.setText("");
+
+                //Calls default constructor to make first instance of game state and makes a deep copy of it
+                BattleShipGameState firstInstance = new BattleShipGameState();
+                BattleShipGameState firstDeepCopy = new BattleShipGameState(firstInstance);
+
+                //Calls each method once
+
+                //Makes second instance of game state and makes a deep copy of it
+
+                //Calls toString on both gameStates
 
             }
         };
